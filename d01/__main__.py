342e748ff2e88afeb912fb09fe0b15b21b28beb9
Kenneth V. Domingo <hello@kvd.studio>
2024-12-01 22:03:07 +0800

    feat: day 1 part 2

--- d01/__main__.py
+++ d01/__main__.py
@@ -1,9 +1,13 @@
 from common.config import BASE_DIR
 
 
-def main():
+def load_input():
     with open(BASE_DIR / "d01/input.txt") as fh:
-        data = [l.strip().split("   ") for l in fh.readlines()]
+        return [l.strip().split("   ") for l in fh.readlines()]
+
+
+def part1():
+    data = load_input()
 
     left = []
     right = []
@@ -18,9 +22,27 @@ def main():
     for l, r in zip(left, right, strict=False):
         diff_sum += abs(l - r)
 
-    print(diff_sum)
+    print(f"{diff_sum=}")
     return diff_sum
 
 
+def part2():
+    data = load_input()
+
+    left = []
+    right = []
+    for l, r in data:
+        left.append(int(l))
+        right.append(int(r))
+
+    similarity = 0
+    for l in left:
+        similarity += sum([r for r in right if r == l])
+
+    print(f"{similarity=}")
+    return similarity
+
+
 if __name__ == "__main__":
-    main()
+    part1()
+    part2()
