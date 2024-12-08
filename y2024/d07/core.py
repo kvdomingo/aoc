29f95e15772b3b5652047524adc9e0e412ab55ee
Kenneth V. Domingo <hello@kvd.studio>
2024-12-08 16:05:46 +0800

    feat(2024): day 7 part 1

---
+++ y2024/d07/core.py
@@ -0,0 +1,58 @@
+from itertools import product
+from operator import add, mul
+
+from aocd.models import Puzzle
+
+from common.config import BASE_DIR
+
+puzzle = Puzzle(year=2024, day=7)
+
+
+def load_input(is_test: bool = False) -> str:
+    if is_test:
+        with open(BASE_DIR / "y2024/d07/test.txt") as fh:
+            return fh.read().strip()
+
+    return puzzle.input_data
+
+
+def preprocess_input(data: str):
+    out = []
+    for dat in data.split("\n"):
+        ans, vals_ = tuple(dat.split(": "))
+        out.append({"key": int(ans), "values": [int(v) for v in vals_.split(" ")]})
+
+    return out
+
+
+def part1(data: str):
+    calibrations = preprocess_input(data)
+    pool = "+*"
+    sum_ = 0
+
+    for calibration in calibrations:
+        test_value = calibration["key"]
+        for ops in product(pool, repeat=len(calibration["values"]) - 1):
+            test_result = calibration["values"][0]
+            for i, op in enumerate(ops):
+                match op:
+                    case "+":
+                        operator = add
+                    case "*":
+                        operator = mul
+                    case _:
+                        raise ValueError(f"Unknown {op=} in {ops=}")
+
+                test_result = operator(test_result, calibration["values"][i + 1])
+
+            if test_result == test_value:
+                sum_ += test_value
+                # once we have a working equation, we can skip the rest of the combinations
+                break
+
+    print(f"{sum_=}")
+    return sum_
+
+
+def part2(data: str):
+    pass
--- y2024/tests/test_d07.py
+++ y2024/tests/test_d07.py
@@ -1,9 +1,9 @@
-from y2024.d06.core import load_input, part1
+from y2024.d07.core import load_input, part1
 
 
 def test_part1():
     data = load_input(is_test=True)
-    assert part1(data) == 41
+    assert part1(data) == 3749
 
 
 def test_part2():
