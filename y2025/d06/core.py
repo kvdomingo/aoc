6c8e60decd7e63495ac0ed7d834f35f2843a2fcc
Kenneth V. Domingo <hello@kvd.studio>
2025-12-06 20:01:46 +0800

    feat(2025): day 6 part 2

--- y2025/d06/core.py
+++ y2025/d06/core.py
@@ -13,10 +13,13 @@ puzzle = Puzzle(
 )
 
 
-def load_input(is_test: bool = False) -> str:
+def load_input(is_test: bool = False, strip_extra_whitespace: bool = True) -> str:
     if is_test:
         with open(settings.BASE_DIR / "y2025/d06/test.txt") as fh:
-            return fh.read().strip()
+            read = fh.read()
+            if strip_extra_whitespace:
+                read = read.strip()
+            return read
 
     return puzzle.input_data
 
@@ -40,4 +43,35 @@ def part1(data: str):
 
 
 def part2(data: str):
-    pass
+    grid = [[r.strip() for r in list(line)] for line in data.splitlines()]
+    row_size = len(grid[0])
+    col_size = len(grid)
+    results = []
+
+    sub_problem = ""
+    problem = list[int]()
+    for x in range(row_size - 1, -1, -1):
+        # concat all numbers in column except last row
+        for y in range(col_size - 1):
+            sub_problem += grid[y][x]
+
+        # entire column is empty, skip to next iteration
+        if sub_problem == "":
+            continue
+
+        # append to problem and reset sub_problem string
+        problem.append(int(sub_problem))
+        sub_problem = ""
+
+        # if last row has no operator, skip to next iteration
+        if not grid[-1][x]:
+            continue
+
+        # compute the problem
+        op = sum if grid[-1][x] == "+" else prod
+        results.append(op(problem))
+        problem = list[int]()
+
+    result = sum(results)
+    print(f"{result=}")
+    return result
--- y2025/tests/test_d06.py
+++ y2025/tests/test_d06.py
@@ -2,10 +2,10 @@ from y2025.d06.core import load_input, part1, part2
 
 
 def test_part1():
-    data = load_input(is_test=True)
+    data = load_input(is_test=True, strip_extra_whitespace=False)
     assert part1(data) == 4277556
 
 
 def test_part2():
-    data = load_input(is_test=True)
-    assert part2(data) is None
+    data = load_input(is_test=True, strip_extra_whitespace=False)
+    assert part2(data) == 3263827
