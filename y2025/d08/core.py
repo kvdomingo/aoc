9673f02bffb48d84de5bd0614e07285bcca0c9f8
Kenneth V. Domingo <hello@kvd.studio>
2025-12-12 23:56:42 +0800

    wip(2025): day 8 part 1

--- y2025/d08/core.py
+++ y2025/d08/core.py
@@ -1,4 +1,8 @@
-from math import prod
+from itertools import combinations
+from math import (
+    dist as distance,
+    prod,
+)
 from pprint import PrettyPrinter
 
 from aocd.models import Puzzle, User
@@ -21,46 +25,47 @@ def load_input(is_test: bool = False) -> str:
 
 
 def part1(data: str):
-    vectors = [
+    junctions = [
         tuple[int, int, int](map(int, line.split(","))) for line in data.splitlines()
     ]
+    distances = {(a, b): distance(a, b) for a, b in combinations(junctions, 2)}
+    distances = dict(sorted(distances.items(), key=lambda d: d[1]))
+    closest_junction_pairs = list(distances.keys())[:1000]
+
     circuit_junction_lut: dict[int, set[tuple[int, int, int]]] = {}
     junction_circuit_lut: dict[tuple[int, int, int], int] = {}
+    next_circuit_id = 0
 
-    for i, vec in enumerate(vectors):
-        new_circuit = i
-        circuit_junction_lut[new_circuit] = {vec}
-        junction_circuit_lut[vec] = new_circuit
+    for a, b in closest_junction_pairs:
+        circuit_a = junction_circuit_lut.get(a, None)
+        circuit_b = junction_circuit_lut.get(b, None)
 
-    for a in range(len(vectors)):
-        min_dist = float("inf")
-        closest = None
-        for b in range(a + 1, len(vectors)):
-            dist = sum((vectors[a][i] - vectors[b][i]) ** 2 for i in range(3))
-            if dist < min_dist:
-                min_dist = dist
-                closest = vectors[b]
-
-        if closest is None:
-            continue
-
-        circuit_a = junction_circuit_lut[vectors[a]]
-        circuit_b = junction_circuit_lut[closest]
-        if circuit_a != circuit_b:
-            circuit_junction_lut[circuit_a].update(circuit_junction_lut[circuit_b])
-            for vec in circuit_junction_lut[circuit_b]:
-                junction_circuit_lut[vec] = circuit_a
-            del circuit_junction_lut[circuit_b]
+        if circuit_a is None and circuit_b is None:
+            new_circuit = next_circuit_id
+            next_circuit_id += 1
+            junction_circuit_lut[a] = junction_circuit_lut[b] = new_circuit
+            circuit_junction_lut[new_circuit] = {a, b}
+        elif circuit_a is None:
+            assert circuit_b is not None
+            junction_circuit_lut[a] = circuit_b
+            circuit_junction_lut[circuit_b].add(a)
+        elif circuit_b is None:
+            assert circuit_a is not None
+            junction_circuit_lut[b] = circuit_a
+            circuit_junction_lut[circuit_a].add(b)
+        else:
+            if circuit_a != circuit_b:
+                circuit_junction_lut[circuit_a].update(circuit_junction_lut[circuit_b])
+                for junc in circuit_junction_lut[circuit_b]:
+                    junction_circuit_lut[junc] = circuit_a
+                del circuit_junction_lut[circuit_b]
 
     pp = PrettyPrinter(indent=2)
-    print("circuit_junction_lut")
-    pp.pprint(circuit_junction_lut)
-    print("junction_circuit_lut")
-    pp.pprint(junction_circuit_lut)
-
     sorted_circuits = sorted(
         circuit_junction_lut.values(), key=lambda v: len(v), reverse=True
     )
+    print("sorted_circuits")
+    pp.pprint(sorted_circuits)
     largest_circuits = sorted_circuits[:3]
     size = prod(len(c) for c in largest_circuits)
     print(f"{size=}")
